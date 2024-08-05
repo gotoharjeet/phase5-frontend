00b128a85320c04e2a57aabe516136edb19b97b5
Harjeet Singh <hskainth@itdc.co.in>
2024-08-05 05:32:29 +0530

    committed: Food Details

---
+++ responsive-projects/Project/src/app/home/home.component.ts
@@ -0,0 +1,26 @@
+import { Foods } from '../shared/models/food';
+import { FoodService } from './../services/food/food.service';
+import { Component, OnInit } from '@angular/core';
+import { StarRatingComponent } from 'ng-starrating';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+    // foods:Foods[]=[];
+    foods:Foods[]=[];
+    constructor(private fs:FoodService)
+    {
+
+      
+    }
+    ngOnInit(): void
+      {
+        this.foods=this.fs.getAll();
+      }
+
+
+}
